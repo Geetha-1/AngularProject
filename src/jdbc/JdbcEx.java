9d41bbc907cdb4cddf89c84f9dede175faa50141
Geetha-1 <thotageetha299@gmail.com>
2022-12-29 15:11:44 +0530

    adding project

---
+++ src/jdbc/JdbcEx.java
@@ -0,0 +1,15 @@
+package jdbc;
+
+import java.sql.*;
+
+public class JdbcEx 
+{
+
+	public static void main(String[] args)throws Exception{
+		// TODO Auto-generated method stub
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/java","root","root");
+		System.out.println("Connected..");
+	}
+
+}
